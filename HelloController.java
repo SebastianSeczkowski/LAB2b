d54bee71a4c3921ed894a280a1d55092ba5bef86
SebastianSeczkowski <37137@studentwsei.pl>
2022-06-02 01:23:48 +0200

    uzupełnienie public void btnOpenFileAction(ActionEvent actionEvent)

---
+++ HelloController.java
@@ -0,0 +1,18 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+    }
+}
