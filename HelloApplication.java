613487c2f4d41c3b59ee60814fb0b88f0ce94021
SebastianSeczkowski <37137@studentwsei.pl>
2022-06-03 00:01:45 +0200

    zrealizowanie punktu 6

--- HelloApplication.java
+++ HelloApplication.java
@@ -2,6 +2,7 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,6 +12,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gusInfoGraphic.fxml"));
+        FXMLLoader loader;
+        Parent root = loader.load();
+        ModuleLayer.Controller controller = loader.getController();
+        
+        controller.setHostServices(this.getHostServices());
+        Object primaryStage;
+        controller.setStage(primaryStage);
+        
         Scene scene = new Scene(fxmlLoader.load(), 800, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
